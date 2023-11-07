51fd759acab0f9d7f190563c925ff0f13b65b6e1
Radu-Antonio <raduantonio_2525@yahoo.com>
2023-11-07 15:52:21 +0200

    hw 2 done

--- homework2/Homework2.md
+++ homework2/Homework2.md
@@ -1,7 +1,12 @@
-# Homework 1 - LED RGB
+# Homework 2 - Elevator simulator wannabe
 
-This assignment focuses on controlling each channel of an RGB LED using individual potentiometers.
+This assignment involves simulating a 3-floor elevator control system using LEDs and buttons with Arduino.
+
+# How it works
+
+If the elevator is already at the desired floor, pressing the button for that floor would have no effect. 
+Otherwise, after a button press, the elevator will move to the corresponding floor. If the elevator is in movement, a button press will do nothing.
 
 ![im1](https://github.com/Radu-Antonio/IntroductionToRobotics/assets/93879460/0b5b7817-6318-457d-9490-4982be610e48)
 
-[demo](https://www.youtube.com/watch?v=V2Z1-XqeLN0)
+[demo](https://www.youtube.com/shorts/X9_kdsqPf4I)
--- homework2/homework2.ino
+++ homework2/homework2.ino
@@ -1,36 +1,119 @@
-const int elevatorBlinker = 4;
-const int level1Pin = 10;
-const int level2Pin = 11;
-const int level3Pin = 12;
-const int level1Button = 5;
-const int level2Button = 6;
-const int level3Button = 7;
-int currentLevel = 1;
+const int buttonPin1 = 10;
+const int buttonPin2 = 9;
+const int buttonPin3 = 8;
+
+const int buttons[] = {buttonPin1, buttonPin2, buttonPin3};
+
+const int level1Pin = 7;
+const int level2Pin = 6;
+const int level3Pin = 5;
+
+const int levels[] = {level1Pin, level2Pin, level3Pin};
+
+const int blinkerPin = 4;
+const int blinkDelay = 200;
+int lastBlink = 0;
+byte blinkerState = HIGH;
+
+byte buttonState[] = {HIGH, HIGH, HIGH};
+byte ledState[] = {LOW, LOW, LOW};
+byte buttonReading[] = {HIGH, HIGH, HIGH};
+byte lastButtonReading[] = {HIGH, HIGH, HIGH};
+
+const int debounceDelay = 60;
+unsigned long int lastDebounce[] = {0, 0, 0};
+
+byte i = 0;
+byte currentLevel = 0;
+byte nextLevel = 0;
 bool isMoving = false;
 
+const int levelMovingDelay = 1000;
+int lastMovingTime = 0;
+
 void setup() {
-  int buttons[] = { level1Button, level2Button, level3Button };
-  pinMode(elevatorBlinker, OUTPUT);
-  pinMode(level1Pin, OUTPUT);
-  pinMode(level2Pin, OUTPUT);
-  pinMode(level3Pin, OUTPUT);
   for (int i = 0; i < 3; i++) {
-    pinMode(buttons[i], INPUT);
+    pinMode(buttons[i], INPUT_PULLUP);
+    pinMode(levels[i], OUTPUT);
   }
-  digitalWrite(level1Pin, HIGH);
-  digitalWrite(elevatorBlinker, HIGH);
+  pinMode(blinkerPin, OUTPUT);
+  ledState[currentLevel] = HIGH;
+  digitalWrite(levels[currentLevel], HIGH);
 }
 
+// in superLOOP daca nu ne miscam verificam butoanele in cazul in care cineva cheama liftul
+// daca se intample sa-l apese liftul se pune in miscare, iar cat timp este in miscare nu mai verificam butoanele
+// ci mutam liftul de la nivelul curent la nivelul butonului apasat
 void loop() {
+  blink();
   if (!isMoving) {
-    for (button in buttons)
-      if (buttonPressed(button))
-        if (button != currentLevel) {
-          // move to the wanted level
-          isMoving = true;
-        }
+    for (int i = 0; i < 3; i++) {
+      checkButton(i);
+    }
   } else {
-    blink(elevatorBlinker);
+    moveElevator(currentLevel, nextLevel);
+  }
+}
+
+void blink() {
+  if (!isMoving) {
+    blinkerState = HIGH;
+  } else if (millis() - lastBlink >= blinkDelay) {
+    lastBlink = millis();
+    blinkerState = !blinkerState;
+  }
+  digitalWrite(blinkerPin, blinkerState);
+}
+
+// verfica pentru un buton dat daca state ul s-a schimbat. daca da pornim un timer si daca
+// dupa o perioada de 50 ms state ul citit e diferit de state ul butonului inseamna ca butonul si-a schimat starea
+// si atunci verificam daca s-a apasat pe buton si daca nu s a apasat la nivelul curent, atunci tinem minte nivelul 
+// destinatie si punem liftul in miscare
+void checkButton(int idx) {
+  buttonReading[idx] = digitalRead(buttons[idx]);
+
+  if (buttonReading[idx] != lastButtonReading[idx]) {
+    lastDebounce[idx] = millis();
+  }
+
+  if (millis() - lastDebounce[idx] >= debounceDelay) {
+    if (buttonReading[idx] != buttonState[idx]) {
+      buttonState[idx] = buttonReading[idx];
+      if (buttonState[idx] == LOW && currentLevel != idx) {
+        nextLevel = idx;
+        isMoving = true;
+        lastMovingTime = millis();
+      }
+    }
+  }
+  digitalWrite(levels[idx], ledState[idx]);
+  lastButtonReading[idx] = buttonReading[idx];
+}
+
+// o data la un interval de o secunde mutam liftul cu un nivel, daca nivelul destinatie
+// este mai sus, ne ducem cu un nivel in sus, daca e in jos, ne ducem un nivel in jos
+// cand nivelul curent == nivelul destinatie, am ajuns :)
+void moveElevator(byte &currentLevel, byte nextLevel) {
+  if (millis() - lastMovingTime >= levelMovingDelay) {
+    lastMovingTime = millis();
+
+    if (nextLevel - currentLevel > 0) { // urcam
+      ledState[currentLevel] = LOW;
+      digitalWrite(levels[currentLevel], LOW);
+      currentLevel++;
+      ledState[currentLevel] = HIGH;
+      digitalWrite(levels[currentLevel], HIGH);
     
+    } else if (nextLevel - currentLevel < 0) { // coboram
+      ledState[currentLevel] = LOW;
+      digitalWrite(levels[currentLevel], LOW);
+      currentLevel--;
+      ledState[currentLevel] = HIGH;
+      digitalWrite(levels[currentLevel], HIGH);
+    
+    } else { // am ajuns
+      isMoving = false;
+      return;
+    }
   }
 }
